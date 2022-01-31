5500cf40b857b1ac68a9070e4eb2e2536d2393ac
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-01-31 11:52:14 +0200

    Connect to MongoDB.

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
 const express = require('express');
 
 const app = express();
+const mongoose = require('mongoose');
+require('dotenv/config');
 
 // ROUTES
 app.get('/', (req, res) => {
   res.send('We are on home');
 });
+
+// Connect to DB
+mongoose.connect(process.env.DB_CONNECTION, () =>
+  console.log('Connected to DB')
+);
 
 app.listen(3000);
