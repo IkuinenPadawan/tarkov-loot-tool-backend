86dc63103e7951e445b47f66546a0359e0d20ba6
Juuso Melentjeff <juuso.melentjeff@gmail.com>
2022-02-04 10:45:41 +0200

    Add GET to find module based on id.

--- routes/modules.js
+++ routes/modules.js
@@ -11,4 +11,14 @@ router.get('/', async (req, res) => {
   }
 });
 
+// SPECIFIC MODULE
+router.get('/:moduleId', async (req, res) => {
+  try {
+    const module = await Module.find({ id: req.params.moduleId }, { _id: 0 });
+    res.json(module);
+  } catch (err) {
+    res.json({ message: err });
+  }
+});
+
 module.exports = router;
